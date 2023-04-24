c96d034981320b9f5babc71f2ef3b6af0460b31e
虚无XuWu <2352530877@qq.com>
2023-04-25 00:59:03 +0800

    Update

--- main.c
+++ main.c
@@ -16,7 +16,7 @@ int main() {
     char inputpassword[100];
     char user[usernum][100];
     char password[usernum][100];
-    int borrowed_books[9] = {0};
+//    int borrowed_books[9] = {0};
     char books[9][100] = {"数学", "语文", "英语", "化学", "物理", "生物","政治", "历史", "地理"};
     printf("欢迎使用图书管理系统!\n");
     for (choice = 0; choice != 48;) {
@@ -25,6 +25,9 @@ int main() {
         printf("2.登录\n");
         printf("3.借书\n");
         printf("4.还书\n");
+        if (islogin == 1) {
+            printf("5.注销\n");
+        }
         printf("0.退出程序\n");
         a = getchar();
         getchar();
@@ -44,12 +47,20 @@ int main() {
                     getchar();
                     break;
                 case 50: //50是2的ASCII码
+                    if (usernum == 0) {
+                        printf("请先注册!\n");
+                        break;
+                    }
+                    if (islogin == 1) {
+                        printf("您已经登入了!\n");
+                        break;
+                    }
                     printf("请输入您的用户名:");
                     scanf("%s", inputuser);
                     printf("请输入您的密码:");
                     scanf("%s", inputpassword);
                     for (int i = 0; i < usernum; i++) {
-                        if (strcmp(user[i], inputuser) == 0 && strcmp(password[i], inputpassword) == 0) {
+                        if (strcmp(inputuser, user[i]) == 0 && strcmp(inputpassword, password[i]) == 0) {
                             found = 1;
                             break;
                         }
@@ -70,14 +81,16 @@ int main() {
                     printf("本馆目前库存如下:\n");
                     for (int i = 0; i < 9; i++) {
                         printf("%d. %s", i+1, books[i]);
-                        if (borrowed_books[i]) {
-                            printf(" (borrowed)");
-                        }
                         printf("\n");
                     }
+                    printf("请输入您要借阅的书籍编号:");
 //                    getchar();
                     break;
                 case 52: //52是4的ASCII码
+                    break;
+                case 53: //53是5的ASCII码
+                    printf("注销成功!\n");
+                    islogin = 0;
                     break;
                 case 48: //48是0的ASCII码
                     printf("感谢您的使用!\n");
